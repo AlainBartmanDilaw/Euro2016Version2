6f8ec679c56a2b532964b8019d87dcaa42b30e01
BartmanDilaw <fraternite76-alain@yahoo.fr>
2016-06-14 00:51:38 +0200

    Ajout des images des pays Mise en place partielle du Layout Modification des champs affichés pour les Paris

---
+++ Euro2016/Helpers/AppHelpers.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Euro2016.Helpers
+{
+    public static class CustomHtmlHelpers
+    {
+        public static IHtmlString ImageActionLink(this HtmlHelper htmlHelper,
+        string linkText, string action, string controller,
+        object routeValues, object htmlAttributes, string imageSrc)
+        {
+            var urlHelper = new UrlHelper(htmlHelper.ViewContext.RequestContext);
+            var img = new TagBuilder("img");
+            img.Attributes.Add("src", VirtualPathUtility.ToAbsolute(imageSrc));
+            var anchor = new TagBuilder("a") { InnerHtml = img.ToString(TagRenderMode.SelfClosing) };
+            anchor.Attributes["href"] = urlHelper.Action(action, controller, routeValues);
+            anchor.MergeAttributes(new RouteValueDictionary(htmlAttributes));
+
+            return MvcHtmlString.Create(anchor.ToString());
+        }
+    }
+
+    public static class ImageHelper
+    {
+        public static MvcHtmlString Image(this HtmlHelper helper, string src, string altText, string height)
+        {
+            var builder = new TagBuilder("img");
+            builder.MergeAttribute("src", src);
+            builder.MergeAttribute("alt", altText);
+            builder.MergeAttribute("height", height);
+            return MvcHtmlString.Create(builder.ToString(TagRenderMode.SelfClosing));
+        }
+    }
+}
+
